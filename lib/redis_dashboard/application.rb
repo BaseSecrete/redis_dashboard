007574f99035f8904bbe65c86b6e8c153fce2933
Alexander Logvinov <avl@logvinov.com>
2017-06-13 12:09:34 +0800

    Remove client creation for /application.css and / routes

--- lib/redis_dashboard/application.rb
+++ lib/redis_dashboard/application.rb
@@ -3,7 +3,7 @@ require "redis"
 require "uri"
 
 class RedisDashboard::Application < Sinatra::Base
-  after { client.close }
+  after { client.close if redis_id }
 
   get "/" do
     erb(:index, locals: {clients: clients})
@@ -33,7 +33,7 @@ class RedisDashboard::Application < Sinatra::Base
   end
 
   def client
-    @client ||= RedisDashboard::Client.new(RedisDashboard.urls[redis_id])
+    @client ||= RedisDashboard::Client.new(RedisDashboard.urls[redis_id.to_i])
   end
 
   def clients
@@ -52,7 +52,7 @@ class RedisDashboard::Application < Sinatra::Base
     end
 
     def redis_id
-      params[:id].to_i
+      params[:id]
     end
 
     def active_page?(path='')
